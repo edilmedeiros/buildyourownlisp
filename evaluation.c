db15053057727ab0903a6769c407262215391db9
Edil Medeiros <jose.edil@gmail.com>
2023-10-21 14:11:05 -0300

    Move files

--- evaluation.c
+++ evaluation.c
@@ -27,6 +27,53 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+int number_of_nodes(mpc_ast_t* t) {
+  if (t->children_num == 0) {
+    printf("Node %s with: %s\n", t->tag, t->contents);
+    return 1;
+  }
+  if (t->children_num >= 1) {
+    int total = 1;
+    printf("Node %s has %d children. Recursing...\n", t->tag, t->children_num);
+    for (int i = 0; i < t->children_num; i++) {
+      total += number_of_nodes(t->children[i]);
+    }
+    return total;
+  }
+  return 0;
+}
+
+long eval_op(long x, char* op, long y) {
+  if(strcmp(op, "+") == 0) { return x + y; }
+  if(strcmp(op, "-") == 0) { return x - y; }
+  if(strcmp(op, "*") == 0) { return x * y; }
+  if(strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it directly */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in 'x' */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main (int argc, char** argv) {
 
   /* Create some Parsers */
@@ -59,7 +106,8 @@ int main (int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On success print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print the error */
